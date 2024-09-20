f273274132f15af5725abc46ba8c0b10b8f77855
hhhscvx <hhhscvx@protonmail.com>
2024-09-20 16:23:25 +0500

    Pydantic settings init

---
+++ data/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env', env_ignore_empty=True)
+
+    API_ID: int
+    API_HASH: str
+
+
+config = Settings()
