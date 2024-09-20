6c812321e230d10b6f0e1616a52080b41a309bf2
hhhscvx <hhhscvx@protonmail.com>
2024-09-20 17:07:43 +0500

    Login functional added

--- utils/banana.py
+++ utils/banana.py
@@ -1,7 +1,13 @@
 
+import asyncio
+import random
+from urllib.parse import quote, unquote
+
 from aiohttp_socks import ProxyConnector
 import aiohttp
 from pyrogram import Client
+from pyrogram.raw.functions.messages import RequestWebView
+from pyrogram.raw.types import WebViewResultUrl
 from fake_useragent import UserAgent
 
 from utils.core import logger
@@ -26,3 +32,50 @@ class Banana:
 
         headers = {"User-Agent": UserAgent(os='android').random}
         self.session = aiohttp.ClientSession(headers=headers, trust_env=True, connector=connector)
+
+    async def logout(self) -> None:
+        await self.session.close()
+
+    async def login(self) -> bool:
+        await asyncio.sleep(random.uniform(*config.DELAY_CONN_ACCOUNT))
+        query = await self.get_tg_web_data()
+
+        if query is None:
+            logger.error(f"{self.session_name} | Session {self.session_name}.session invalid")
+            await self.logout()
+            return
+
+        resp = await self.session.post(url='https://interface.carv.io/banana/login',
+                                       json={'tgInfo': query})
+        resp_json = await resp.json()
+
+        self.session.headers["Authorization"] = resp_json['data']['token']
+        return True
+
+    async def get_tg_web_data(self):
+        try:
+            await self.tg_client.connect()
+            await self.tg_client.send_message('OfficialBananaBot', '/start')
+            await asyncio.sleep(random.uniform(1.5, 2))
+
+            web_view: WebViewResultUrl = await self.tg_client.invoke(RequestWebView(
+                peer=await self.tg_client.resolve_peer('OfficialBananaBot'),
+                bot=await self.tg_client.resolve_peer('OfficialBananaBot'),
+                platform="android",
+                from_bot_menu=False,
+                url="https://banana.carv.io/"
+            ))
+            await self.tg_client.disconnect()
+            auth_url = web_view.url
+
+            query = unquote(string=unquote(string=auth_url.split('tgWebAppData=')[1].split('&tgWebAppVersion')[0]))
+            query_id = query.split('query_id=')[1].split('&user=')[0]
+            user = quote(query.split("&user=")[1].split('&auth_date=')[0])
+            auth_date = query.split('&auth_date=')[1].split('&hash=')[0]
+            hash_ = query.split('&hash=')[1]
+
+            print(f"query_id={query_id}&user={user}&auth_date={auth_date}&hash={hash_}")
+
+            return f"query_id={query_id}&user={user}&auth_date={auth_date}&hash={hash_}"
+        except Exception as err:
+            return
